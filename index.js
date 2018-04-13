dff4cec53972fdbfa920eae1591806a34904c23f
Eirik Backer <Eirik.Backer@nrk.no>
2018-04-13 14:21:56 +0200

    Keep day within month and month within year

--- index.js
+++ index.js
@@ -25,35 +25,43 @@
 // -181 + y0 = -80
 // -181 + y0yy = -81
 // -181 + y0 = -180
+// y-0-0 = first day of year (keeps month within year and day within month)
+// y-4-90 = last day of april (keeps day within month)
+
+const DATE = {year: 'FullYear', month: 'Month', week: 'Date', day: 'Date', hour: 'Hours', minute: 'Minutes', second: 'Seconds'}
+const ADD = /([+-]\s*\d+)\s*(second|minute|hour|day|week|month|year)|(mon)|(tue)|(wed)|(thu)|(fri)|(sat)|(sun)/g
+const YMD = /([-\dy]+)[-/.]([\dm]{1,2})[-/.]([\dd]{1,2})/
+const HMS = /([\dh]{1,2}):([\dm]{1,2}):?([\ds]{1,2})?/
 
 export default function parse (parse, from) {
   if (isFinite(parse)) return new Date(Number(parse)) // Allow timestamps and Date instances
 
   const text = String(parse).toLowerCase()
   const date = new Date((isFinite(from) && text.indexOf('now') === -1) ? Number(from) : Date.now())
-  const name = {year: 'FullYear', month: 'Month', week: 'Date', day: 'Date', hour: 'Hours', minute: 'Minutes', second: 'Seconds'}
-  const math = /([+-]\s*\d+)\s*(second|minute|hour|day|week|month|year)|(mon)|(tue)|(wed)|(thu)|(fri)|(sat)|(sun)/g
-  const [, year = 'y', month = 'm', day = 'd'] = text.match(/([-\dy]+)[-/.]([\dm]{1,2})[-/.]([\dd]{1,2})/) || []
-  const [, hour = 'h', minute = 'm', second = 's'] = text.match(/([\dh]{1,2}):([\dm]{1,2}):?([\ds]{1,2})?/) || []
+  const [, year = 'y', month = 'm', day = 'd'] = text.match(YMD) || []
+  const [, hour = 'h', minute = 'm', second = 's'] = text.match(HMS) || []
   let match = {year, month, day, hour, minute, second}
 
   Object.keys(match).forEach((unit) => {
     const move = unit === 'month' ? 1 : 0 // Month have zero based index
-    const prev = `${date[`get${name[unit]}`]() + move}` // Shift to consistent index
-    const next = match[unit].replace(/[^-\d]+/g, (match, index, next) => { // Replace non digit chars
+    const prev = `${date[`get${DATE[unit]}`]() + move}` // Shift to consistent index
+    match[unit] = match[unit].replace(/[^-\d]+/g, (match, index, next) => { // Replace non digit chars
       if (index) return prev.substr(prev.length - next.length + index, match.length) // Inside: copy match.length
       return prev.substr(0, Math.max(0, prev.length - next.length + match.length)) // Start: copy leading chars
-    })
-
-    date[`set${name[unit]}`](next - move)
+    }) - move
   })
 
-  while ((match = math.exec(text)) !== null) { // match = [fullMatch, number, unit, mon, tue, etc...]
+  // Keep units within boundries
+  const lastDayInMonth = new Date(match.year, Math.min(12, match.month + 1), 0).getDate()
+  date.setFullYear(match.year, Math.min(11, match.month), Math.max(1, Math.min(lastDayInMonth, match.day)))
+  date.setHours(Math.min(24, match.hour), Math.min(59, match.minute), Math.min(59, match.second))
+
+  while ((match = ADD.exec(text)) !== null) { // match = [fullMatch, number, unit, mon, tue, etc...]
     const unit = match[2]
     const size = String(match[1]).replace(/\s/g, '') * (unit === 'week' ? 7 : 1) // Strip whitespace and correct week
     const day = match.slice(2).indexOf(match[0]) // Weekdays starts at 3rd index but is not zero based
 
-    if (unit) date[`set${name[unit]}`](date[`get${name[unit]}`]() + size)
+    if (unit) date[`set${DATE[unit]}`](date[`get${DATE[unit]}`]() + size)
     else date.setDate(date.getDate() - (date.getDay() || 7) + day) // Make sunday 7th day
   }
 
