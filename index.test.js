25a53611014d73351679a2cfd5e6b633bf7ca59c
William Almnes <william.almnes@nrk.no>
2018-04-15 21:55:21 +0200

    add tests

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-/* globals describe, expect, test */
+/* globals describe, expect, test, beforeEach */
 const parse = require('./index.min.js')
 
 describe('parse', () => {
@@ -16,5 +16,252 @@ describe('parse', () => {
     expect(parse(0).getTime()).toBe(jan19170)
     expect(parse(null).getTime()).toBe(jan19170)
     expect(parse(false).getTime()).toBe(jan19170)
+  })
+
+  describe('relative dates', () => {
+    let now
+    beforeEach(() => {
+      now = parse('now').getTime()
+    })
+    test('should be able to add a minute to current time', () => {
+      const parseNext = parse('+1minute', now)
+      const expected = new Date(now).setMinutes((new Date(now).getMinutes() + 1))
+
+      expect(parseNext.getTime()).toBe(expected)
+    })
+    test('should be able to subtract a minute to current time', () => {
+      const parsePrev = parse('-1minute', now)
+      const expected = new Date(now).setMinutes((new Date(now).getMinutes() - 1))
+
+      expect(parsePrev.getTime()).toBe(expected)
+    })
+    test('should be able to add an hour to current time', () => {
+      const praseNext = parse('+1hour', now)
+      const expected = new Date(now).setHours((new Date(now).getHours() + 1))
+
+      expect(praseNext.getTime()).toBe(expected)
+    })
+    test('should be able to subtract an hour to current time', () => {
+      const parsePrev = parse('-1hour', now)
+      const previousDay = new Date(now).setHours((new Date(now).getHours() - 1))
+
+      expect(parsePrev.getTime()).toBe(previousDay)
+    })
+    test('should be able to add a day to current time', () => {
+      const parseNext = parse('+1day', now)
+      const expected = new Date(now).setDate((new Date(now).getDate() + 1))
+
+      expect(parseNext.getTime()).toBe(expected)
+    })
+    test('should be able to subtract a day to current time', () => {
+      const parsePrev = parse('-1day', now)
+      const expected = new Date(now).setDate((new Date(now).getDate() - 1))
+
+      expect(parsePrev.getTime()).toBe(expected)
+    })
+    test('should be able to add a week to current time', () => {
+      const parseNext = parse('+1week', now)
+      const expected = new Date(now).setDate((new Date(now).getDate() + 7))
+
+      expect(parseNext.getTime()).toBe(expected)
+    })
+    test('should be able to subtract a week to current time', () => {
+      const parsePrev = parse('-1week', now)
+      const expected = new Date(now).setDate((new Date(now).getDate() - 7))
+
+      expect(parsePrev.getTime()).toBe(expected)
+    })
+    test('should be able to add a month to current time', () => {
+      const parseNext = parse('+1month', now)
+      const expected = new Date(now).setMonth((new Date(now).getMonth() + 1))
+
+      expect(parseNext.getTime()).toBe(expected)
+    })
+    test('should be able to subtract a month to current time', () => {
+      const parsePrev = parse('-1month', now)
+      const expected = new Date(now).setMonth((new Date(now).getMonth() - 1))
+
+      expect(parsePrev.getTime()).toBe(expected)
+    })
+    test('should be able to add a year to current time', () => {
+      const parseNext = parse('+1year', now)
+      const expected = new Date(now).setFullYear((new Date(now).getFullYear() + 1))
+
+      expect(parseNext.getTime()).toBe(expected)
+    })
+    test('should be able to subtract a year to current time', () => {
+      const parsePrev = parse('-1year', now)
+      const expected = new Date(now).setFullYear((new Date(now).getFullYear() - 1))
+
+      expect(parsePrev.getTime()).toBe(expected)
+    })
+  })
+
+  describe('"from" dates', () => {
+
+  })
+
+  describe('yyyy-mm-dd format', () => {
+    let now
+    beforeEach(() => {
+      now = new Date()
+    })
+
+    test('yyyy-mm-01 should get the current year and month, but set day to the first', () => {
+      const date = parse('yyyy-mm-01')
+
+      expect(date.getFullYear()).toBe(now.getFullYear())
+      expect(date.getMonth()).toBe(now.getMonth())
+      expect(date.getDate()).toBe(1)
+    })
+
+    // NOTE: If the month does not have 31 days, it is set to the maximum date of the month
+    test('yyyy-mm-31 should get the current year and month, but set day to the last day of the month', () => {
+      const date = parse('yyyy-mm-31')
+
+      expect(date.getFullYear()).toBe(now.getFullYear())
+      expect(date.getMonth()).toBe(now.getMonth())
+      expect(date.getDate()).toBe(new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate())
+    })
+
+    test('yyyy-01-dd should get the current year and date, but set month to January', () => {
+      const date = parse('yyyy-01-dd')
+
+      expect(date.getFullYear()).toBe(now.getFullYear())
+      expect(date.getMonth()).toBe(0)
+      expect(date.getDate()).toBe(now.getDate())
+    })
+
+    test('yyyy-2342-dd should get the current year and date, but set month to December', () => {
+      const date = parse('yyyy-14-dd')
+
+      expect(date.getFullYear()).toBe(now.getFullYear())
+      expect(date.getMonth()).toBe(11)
+      expect(date.getDate()).toBe(now.getDate())
+    })
+
+    test('19yy-mm-dd should set the first two digits to 19 and the last two digits to the last two digits of the current year', () => {
+      const date = parse('19yy-mm-dd')
+
+      // Just doing - 100 cause I assume this code won't last into the next century
+      expect(date.getFullYear()).toBe(now.getFullYear() - 100)
+      expect(date.getMonth()).toBe(now.getMonth())
+      expect(date.getDate()).toBe(now.getDate())
+    })
+
+    test('yy01-mm-dd should get the current month and date, but set month to December', () => {
+      const date = parse('yy01-mm-dd')
+
+      expect(date.getFullYear().toString().substr(0, 2)).toBe(now.getFullYear().toString().substr(0, 2))
+      expect(date.getFullYear().toString().substr(2)).toBe('01')
+      expect(date.getMonth()).toBe(now.getMonth())
+      expect(date.getDate()).toBe(now.getDate())
+    })
+
+    test('y25y-mm-dd should get the current year and date, but set month to December', () => {
+      const date = parse('y25y-mm-dd')
+
+      expect(date.getFullYear().toString().charAt(0)).toBe(now.getFullYear().toString().charAt(0))
+      expect(date.getFullYear().toString().charAt(3)).toBe(now.getFullYear().toString().charAt(3))
+      expect(date.getFullYear().toString().substr(1, 2)).toBe('25')
+      expect(date.getMonth()).toBe(now.getMonth())
+      expect(date.getDate()).toBe(now.getDate())
+    })
+  })
+
+  describe('days of the week', () => {
+    let from
+    beforeEach(() => {
+      from = new Date('Sun Apr 15 2018 12:44:03 GMT+0200 (CEST)')
+    })
+
+    test('"monday" should give the previous monday', () => {
+      const date = parse('monday', from)
+      const expectedDate = new Date(from)
+      expectedDate.setDate(from.getDate() - 6)
+      expect(date.getTime()).toBe(expectedDate.getTime())
+    })
+
+    test('"monday" should give the same date if the date is monday', () => {
+      const monday = new Date('Mon Apr 16 2018 12:44:03 GMT+0200 (CEST)')
+      const date = parse('monday', monday)
+      expect(date.getTime()).toBe(monday.getTime())
+    })
+
+    test('"tuesday" should give the previous tuesday', () => {
+      const date = parse('tuesday', from)
+      const expectedDate = new Date(from)
+      expectedDate.setDate(from.getDate() - 5)
+      expect(date.getTime()).toBe(expectedDate.getTime())
+    })
+
+    test('"tuesday" should give the same date if the date is tuesday', () => {
+      const tuesday = new Date('Tue Apr 17 2018 12:44:03 GMT+0200 (CEST)')
+      const date = parse('tuesday', tuesday)
+      expect(date.getTime()).toBe(tuesday.getTime())
+    })
+
+    test('"wednesday" should give the previous wednesday', () => {
+      const date = parse('wednesday', from)
+      const expectedDate = new Date(from)
+      expectedDate.setDate(from.getDate() - 4)
+      expect(date.getTime()).toBe(expectedDate.getTime())
+    })
+
+    test('"wednesday" should give the same date if the date is wednesday', () => {
+      const wednesday = new Date('Wed Apr 18 2018 12:44:03 GMT+0200 (CEST)')
+      const date = parse('wednesday', wednesday)
+      expect(date.getTime()).toBe(wednesday.getTime())
+    })
+
+    test('"thursday" should give the previous thursday', () => {
+      const date = parse('thursday', from)
+      const expectedDate = new Date(from)
+      expectedDate.setDate(from.getDate() - 3)
+      expect(date.getTime()).toBe(expectedDate.getTime())
+    })
+
+    test('"thursday" should give the same date if the date is thursday', () => {
+      const thursday = new Date('Thu Apr 19 2018 12:44:03 GMT+0200 (CEST)')
+      const date = parse('thursday', thursday)
+      expect(date.getTime()).toBe(thursday.getTime())
+    })
+
+    test('"friday" should give the previous friday', () => {
+      const date = parse('friday', from)
+      const expectedDate = new Date(from)
+      expectedDate.setDate(from.getDate() - 2)
+      expect(date.getTime()).toBe(expectedDate.getTime())
+    })
+
+    test('"friday" should give the same date if the date is friday', () => {
+      const friday = new Date('Fri Apr 20 2018 12:44:03 GMT+0200 (CEST)')
+      const date = parse('friday', friday)
+      expect(date.getTime()).toBe(friday.getTime())
+    })
+
+    test('"saturday" should give the previous saturday', () => {
+      const date = parse('saturday', from)
+      const expectedDate = new Date(from)
+      expectedDate.setDate(from.getDate() - 1)
+      expect(date.getTime()).toBe(expectedDate.getTime())
+    })
+
+    test('"saturday" should give the same date if the date is saturday', () => {
+      const saturday = new Date('Sat Apr 21 2018 12:44:03 GMT+0200 (CEST)')
+      const date = parse('saturday', saturday)
+      expect(date.getTime()).toBe(saturday.getTime())
+    })
+
+    test('"sunday" should give the previous sunday', () => {
+      const date = parse('sunday', from)
+      expect(date.getTime()).toBe(from.getTime())
+    })
+
+    test('"sunday" should give the same date if the date is sunday', () => {
+      const sunday = new Date('Sun Apr 22 2018 12:44:03 GMT+0200 (CEST)')
+      const date = parse('sunday', sunday)
+      expect(date.getTime()).toBe(sunday.getTime())
+    })
   })
 })
