6720c0093e8f34612d4783f1938c7814e8423a9a
Eirik Backer <eirik.backer@gmail.com>
2019-02-26 14:47:08 +0100

    Update dependencies, add check for month and leap year correction (#7)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const DATE = {year: 'FullYear', month: 'Month', week: 'Date', day: 'Date', hour: 'Hours', minute: 'Minutes', second: 'Seconds'}
+const DATE = { year: 'FullYear', month: 'Month', week: 'Date', day: 'Date', hour: 'Hours', minute: 'Minutes', second: 'Seconds' }
 const ADD = /([+-]\s*\d+)\s*(second|minute|hour|day|week|month|year)|(mon)|(tue)|(wed)|(thu)|(fri)|(sat)|(sun)/g
 const YMD = /([-\dy]+)[-/.]([\dm]+)[-/.]([\dd]+)/
 const HMS = /([\dh]+):([\dm]+):?([\ds]+)?/
@@ -10,11 +10,11 @@ export default function parse (parse, from) {
   const date = new Date((isFinite(from) && text.indexOf('now') === -1) ? Number(from) : Date.now())
   const [, year = 'y', month = 'm', day = 'd'] = text.match(YMD) || []
   const [, hour = 'h', minute = 'm', second = 's'] = text.match(HMS) || []
-  let match = {year, month, day, hour, minute, second}
+  let match = { year, month, day, hour, minute, second }
 
   Object.keys(match).forEach((unit) => {
     const move = unit === 'month' ? 1 : 0 // Month have zero based index
-    const prev = `${date[`get${DATE[unit]}`]() + move}` // Shift to consistent index
+    const prev = String(date[`get${DATE[unit]}`]() + move) // Shift to consistent index
     match[unit] = match[unit].replace(/[^-\d]+/g, (match, index, next) => { // Replace non digit chars
       if (index) return prev.substr(prev.length - next.length + index, match.length) // Inside: copy match.length
       return prev.substr(0, Math.max(0, prev.length - next.length + match.length)) // Start: copy leading chars
@@ -30,9 +30,13 @@ export default function parse (parse, from) {
     const unit = match[2]
     const size = String(match[1]).replace(/\s/g, '') * (unit === 'week' ? 7 : 1) // Strip whitespace and correct week
     const day = match.slice(2).indexOf(match[0]) // Weekdays starts at 3rd index but is not zero based
+    const before = date.getDate()
 
-    if (unit) date[`set${DATE[unit]}`](date[`get${DATE[unit]}`]() + size)
-    else date.setDate(date.getDate() - (date.getDay() || 7) + day) // Make sunday 7th day
+    if (unit) date[`set${DATE[unit]}`](date[`get${DATE[unit]}`]() + size) // Add day/month/week etc
+    else date.setDate(date.getDate() - (date.getDay() || 7) + day) // Adjust weekday and make sunday 7th day
+
+    // If day of month has changed, we have encountered dfferent length months, or leap year
+    if ((unit === 'month' || unit === 'year') && before !== date.getDate()) date.setDate(0)
   }
 
   return date
